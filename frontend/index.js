45bd56626b08a925f592f811e33648993f5551f6
Yumacorn <ReachRyanMui@gmail.com>
2021-01-30 09:45:31 -0500

    troubleshot bug for form submit

--- frontend/index.js
+++ frontend/index.js
@@ -1,33 +1,46 @@
 const restaurantListBtn = document.querySelector('#restaurant-list')
 const newRestaurantForm = document.querySelector('#new-restaurant-form')
 
-
-
+console.log(newRestaurantForm)
 newRestaurantForm.addEventListener("submit", function(e) {
     e.preventDefault()
-    console.log(newRestaurantForm)
+    console.log(e.target)
+    console.log("ji")
 
-    const input = newRestaurantForm.querySelector("#inputRestaurant").value
-    console.log(input)
-    fetch('http://localhost:3000/restaurants', {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json',
-            Accept: 'application/json'
-        },
-        body: JSON.stringify({
-            name: input
-        })
-    })
-    .then(function(response) {
-        return response.json()
-    })
-    .then(function(restaurant){
-        console.log(restaurant)
-    })
 })
+console.log("xyz")
+
+console.log(newRestaurantForm)
+
+
+// newRestaurantForm.addEventListener("submit", function(e) {
+//     console.log(e.target)
+
+
+    // console.log(newRestaurantForm)
+
+    // const input = document.querySelector("#inputRestaurant").value
+    // console.log(input)
+    // fetch('http://localhost:3000/restaurants', {
+    //     method: "POST",
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //         Accept: 'application/json'
+    //     },
+    //     body: JSON.stringify({
+    //         name: input
+    //     })
+    // })
+    // .then(function(response) {
+    //     return response.json()
+    // })
+    // .then(function(restaurant){
+    //     console.log(restaurant)
+    // })
+// })
 
 restaurantListBtn.addEventListener("click", function(e) {
+    console.log(e.target)
     fetch('http://localhost:3000/restaurants')
     .then(function(response){
         return response.json()
