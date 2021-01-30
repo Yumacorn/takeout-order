5667780dea86d583bbfb7fdee8a09d30f1ef24dd
Yumacorn <ReachRyanMui@gmail.com>
2021-01-30 17:37:31 -0500

    save

--- frontend/cart.js
+++ frontend/cart.js
@@ -1,7 +1,13 @@
 let addCartItem = document.getElementsByClassName('add-cart-item')
 let removeCartItem = document.getElementsByClassName('remove-cart-item')
 
-
+for (btn of addCartItem) {
+    btn.addEventListener('click', function(event) {
+        let addButton = event.target
+        console.log(addButton)
+        console.log(addButton.parentElement.value)
+    })
+}
 
 //removal of cart items
 for (btn of removeCartItem) {
