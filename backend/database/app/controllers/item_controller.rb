b892aadc09b75c469c46ad19228d8944999191b2
Yumacorn <ReachRyanMui@gmail.com>
2021-01-30 12:50:06 -0500

    updated Item create action params

--- backend/database/app/controllers/item_controller.rb
+++ backend/database/app/controllers/item_controller.rb
@@ -1,8 +1,16 @@
 class ItemController < ApplicationController
 
     def index
+        @items = Item.all
+
+        serialized_data = ItemSerializer.new(@items).serialized_json
+        render json: serialized_data
     end
 
     def create
+        @item = Item.create(name: params[:name], price: params[:price], specialty: params[:specialty], restaurant_id: params[:restaurant_id])
+
+        serialized_data = ItemSerializer.new(@item).serialized_json
+        render json: serialized_data
     end
 end
