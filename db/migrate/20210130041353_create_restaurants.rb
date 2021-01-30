6e3ae879295cbd4929ae470321bfd929e4b49aba
Yumacorn <ReachRyanMui@gmail.com>
2021-01-29 23:19:15 -0500

    migrated restaurants into database

--- db/migrate/20210130041353_create_restaurants.rb
+++ db/migrate/20210130041353_create_restaurants.rb
@@ -2,7 +2,7 @@ class CreateRestaurants < ActiveRecord::Migration[6.0]
   def change
     create_table :restaurants do |t|
       t.string :name
-      t.boolean :fast-food
+      t.boolean :fastfood
 
       t.timestamps
     end
