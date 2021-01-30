cacda8bd6e1427e5365b76bf44d1d1697b1f4ff2
Yumacorn <ReachRyanMui@gmail.com>
2021-01-30 00:11:41 -0500

    added displaying of multiple elements per Restaurant class

--- frontend/index.js
+++ frontend/index.js
@@ -1,14 +1,22 @@
-const restaurantContainer = document.querySelector('#restaurant-container')
 const restaurantListBtn = document.querySelector('#restaurant-list')
 
 
-console.log(restaurantListBtn)
-
-fetch('http://localhost:3000/restaurants')
+restaurantListBtn.addEventListener("click", function(e) {
+    fetch('http://localhost:3000/restaurants')
     .then(function(response){
         return response.json()
     })
     .then(function(restaurants){
         // restaurantContainer.innerHTML = restaurants.data[0].attributes.name
-        console.log(restaurants.data[0].attributes.name)
+        // console.log(restaurants.data[0].attributes.name)
+        const restaurantContainer = document.querySelector('#restaurant-container')
+        restaurantContainer.innerText = ''
+
+        restaurants.data.forEach(function(restaurant) {
+            const newRestaurant = document.createElement('p')
+            newRestaurant.innerText = restaurant.attributes.name
+            restaurantContainer.appendChild(newRestaurant)
+        })
     })
+})
+
