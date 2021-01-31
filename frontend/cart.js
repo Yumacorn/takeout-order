43656b1effd8625adcbdecbe1ec0143caa237e46
Yumacorn <ReachRyanMui@gmail.com>
2021-01-31 00:59:22 -0500

    clear Cart on purchase

--- frontend/cart.js
+++ frontend/cart.js
@@ -2,12 +2,12 @@ let addCartItem = document.getElementsByClassName('add-cart-item')
 let removeCartItem = document.getElementsByClassName('remove-cart-item')
 const cart= document.querySelector('#order-items')
 const cartTotal = document.querySelector('#order-total-price')
+const totalItems = document.querySelector('#totalItems')
+const purchase = document.querySelector('#purchase')
 
 function addLineItem(item, price, restId) {
     console.log(`"${item}" - ${price} - ${restId}`)
     const newLineItem = document.createElement('div')
-    // newLineItem.className = "order-item"
-    // newLineItem.innerHTML = `"${item}" - ${price} - ${restId}`
     const newLineItemName = document.createElement('span')
     newLineItemName.className = "order-item"
     newLineItemName.innerHTML = `${item}`
@@ -16,10 +16,7 @@ function addLineItem(item, price, restId) {
     newLineItemPrice.innerHTML = `${price}`
     const newLineItemRestaurantId = document.createElement('span')
     newLineItemRestaurantId.className = "order-item"
-    newLineItemRestaurantId.innerHTML = `${restId}`
-    // <span class="cart-header">Item</span>
-    // <span class="cart-header">Price</span>
-    // <span class="cart-header">Restaurant</span>
+    newLineItemRestaurantId.innerHTML = `Restaurant # ${restId}`
     const removeButton = document.createElement('button')
     removeButton.className = "remove-cart-item"
     removeButton.innerText = "X"
@@ -30,6 +27,7 @@ function addLineItem(item, price, restId) {
         removePrice = -1 * parseFloat(price.split('$')[1])
         debugger
         updateCartTotal(removePrice)
+        updateItemTotal(-1)
         removeButton.parentElement.remove()
     })
     newLineItem.appendChild(newLineItemName)
@@ -38,6 +36,7 @@ function addLineItem(item, price, restId) {
 
     cart.appendChild(newLineItem)
     updateCartTotal(price)
+    updateItemTotal(1)
 }
 
 function updateCartTotal(adjPrice) {
@@ -51,4 +50,31 @@ function updateCartTotal(adjPrice) {
     curTotal = curTotal.toFixed(2)
     cartTotal.innerHTML = `$${curTotal}`
     return cartTotal
+}
+
+function updateItemTotal(num) {
+    curItemTotal = totalItems.innerText
+    curNum = parseFloat(curItemTotal)
+    curNum += num
+    totalItems.innerText = curNum
+    return curNum
+}
+purchase.addEventListener('click', purchaseCart)
+
+function purchaseCart() {
+    if (totalItems.innerText == "0") {
+        alert(`Cart is currently empty, please pick an item!`)
+    } else {
+        alert(`Thank you! You've purchased ${totalItems.innerText} items, totaling ${cartTotal.innerText}`)
+        // x = parseFloat(totalItems.innerText)
+        // updateItemTotal(x * -1)
+        totalItems.innerText = "0"
+        cartTotal.innerText = "$0.00"
+        for (let i = cart.children.length; i > 0; i--) {
+            console.log(cart.children)
+            console.log(`On ${i.value}`)
+            cart.remove(i)
+            console.log(`removed ${i}`)
+        }
+    }
 }
